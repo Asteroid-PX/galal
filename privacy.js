5a3fdab1e157cc9326da8d857d3947d582e7337d
Asteroid-PX <merch.abdalrhman@gmail.com>
2025-07-28 11:03:44 +0300

    privacy

---
+++ privacy.js
@@ -0,0 +1,19 @@
+//  Accordion functionality
+const accordionItems = document.querySelectorAll(".accordion");
+console.log(accordionItems, "accordion items");
+
+accordionItems.forEach((item) => {
+  const accordionButton = item.querySelector(".accordion-item");
+  const chevronIcon = item.querySelector(".chevron-icon");
+  const content = item.querySelector(".accordion-content");
+  accordionButton.addEventListener("click", () => {
+    content.classList.toggle("accordion-active");
+    chevronIcon.classList.toggle("chevron-active");
+    if (content.classList.contains("accordion-active")) {
+      item.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  });
+});
